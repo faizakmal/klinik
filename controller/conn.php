f69719ad9b73e1d8ae2a2be2ee10f78b4d0dce74
faizakmal <muhammad.faiz-13@fst.unair.ac.id>
2020-04-07 14:10:29 +0700

    Update conn.php

--- controller/conn.php
+++ controller/conn.php
@@ -1,7 +1,7 @@
 <?php
  
 //MySQLi Procedural
-$conn = mysqli_connect("db4free.net","rootnfbs","kliniknfbs","klinik");
+$conn = mysqli_connect("db4free.net","rootnfbs","kliniknfbs","kliniknfbs");
 if (!$conn) {
 	die("Connection failed: " . mysqli_connect_error());
 }
